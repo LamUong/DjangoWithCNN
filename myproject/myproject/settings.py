bf93acc6e6f8c6b14e252670dadceaac7b13f049
Ubuntu <ubuntu@ip-172-31-14-179.us-west-1.compute.internal>
2016-08-19 22:14:22 +0000

    update

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -121,3 +122,9 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+'''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+'''
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static'),
+)
